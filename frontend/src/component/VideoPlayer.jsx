570f9a155dac040617f1ff4264dd42f7a7ab920e
shawnmarkus <shawnmarkus4200@gmail.com>
2023-03-12 11:45:39 +0530

    added like feature

--- frontend/src/component/VideoPlayer.jsx
+++ frontend/src/component/VideoPlayer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import data from "../data/Videolist.json";
@@ -11,33 +11,43 @@ const VideoPlayer = () => {
 
   const [commentData, setCommentData] = useState([]);
 
-  // comment data
-  // const commentData = [
-  //   {
-  //     userId: "02b",
-  //     comId: "017",
-  //     fullName: "Lily",
-  //     userProfile: "https://www.linkedin.com/in/riya-negi-8879631a9/",
-  //     text: "I think you have a point",
-  //     avatarUrl: "https://ui-avatars.com/api/name=Lily&background=random",
-  //     replies: [],
-  //   },
-  // ];
+  // const isLiked = useRef();
+  const [isLiked, setIsLiked] = useState(false);
 
-  // useEffect(() => {
-  //   console.log("index=> ", videoId, idx);
-  // });
+  const handleLikeBtn = () => {
+    if (!isLiked) {
+      console.log("liked it");
+      localStorage.setItem("likedVideo", JSON.stringify([videoId]));
+      setIsLiked(true);
+      data[idx]["noOfLikes"] = 1 + data[idx]["noOfLikes"];
+    }
+  };
 
   useEffect(() => {
     try {
       const data = localStorage.getItem(videoId);
       const jsonData = JSON.parse(data);
-      // console.log("item digged out", JSON.stringify(jsonData, null, 2));
       setCommentData(jsonData);
     } catch (e) {
       console.log(e);
     }
   }, []);
+
+  useEffect(() => {
+    const retrieveData = JSON.parse(localStorage.getItem("likedVideo"));
+    console.log("retrieved data", retrieveData);
+    if (retrieveData) {
+      console.log(
+        "print kr bhai",
+        retrieveData.filter((itrId) => itrId === videoId)
+      );
+      setIsLiked(
+        retrieveData.filter((itrId) => itrId === videoId) ? true : false
+      );
+    } else {
+      setIsLiked(false);
+    }
+  }, [isLiked]);
 
   return (
     <div className="container ">
@@ -48,6 +58,24 @@ const VideoPlayer = () => {
         height="100%"
         url={data[idx]["url"]}
       />
+
+      <div className={Style.likeBtn}>
+        <div className="d-flex" onClick={handleLikeBtn}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+            height="30"
+            width="30"
+          >
+            <path
+              fill={isLiked ? "blue" : ""}
+              d="M35.8 42H13.6V16.4L27.5 2l1.95 1.55q.3.25.45.7.15.45.15 1.1v.5L27.8 16.4h14.95q1.2 0 2.1.9.9.9.9 2.1v4.1q0 .35.075.725t-.075.725l-6.3 14.5q-.45 1.05-1.475 1.8Q36.95 42 35.8 42Zm-19.2-3h19.85l6.3-14.95V19.4H24.1l2.65-12.45-10.15 10.7Zm0-21.35V39Zm-3-1.25v3H6.95V39h6.65v3H3.95V16.4Z"
+            />
+          </svg>
+          &nbsp;
+          <p>{data[idx]["noOfLikes"]}</p>
+        </div>
+      </div>
 
       <div className={Style.commentsSection}>
         <CommentSection
