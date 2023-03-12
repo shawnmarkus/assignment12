71cd1529b3725f7e71cc45d2a48c7c075ceae208
shawnmarkus <shawnmarkus4200@gmail.com>
2023-03-12 10:08:31 +0530

    removed the unnecessary file

--- backend/connectDb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-dotenv.config({ path: "./config.env" });
-
-mongoose
-  .connect(process.env.DB_LINK)
-  .then((db) => console.log("db is connected"))
-  .catch((err) => console.log("thrown err : " + err.message));
